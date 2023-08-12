7944f6b9b57d78684f3808a26b716851767971da
Simon Zolin <4729655+stsaz@users.noreply.github.com>
2023-08-12 08:06:18 +0300

    + android: Settings

--- android/netmill/src/main/java/com/github/stsaz/netmill/MainActivity.java
+++ android/netmill/src/main/java/com/github/stsaz/netmill/MainActivity.java
@@ -55,17 +55,13 @@ public class MainActivity extends AppCompatActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
 		if (id == R.id.action_settings) {
-			return true;
+			startActivity(new Intent(this, SettingsActivity.class));
+		} else {
+			return super.onOptionsItemSelected(item);
 		}
-
-		return super.onOptionsItemSelected(item);
+		return true;
 	}
 
 	/** Process the result of requestPermissions() */
